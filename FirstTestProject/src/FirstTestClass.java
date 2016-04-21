c9ddbd127c20b06d03a0732ed9a2d33dfad1790d
charliewang01 <wgcwg01@163.com>
2016-04-21 14:52:54 +0800

    New Commit by charliewong

--- FirstTestProject/src/FirstTestClass.java
+++ FirstTestProject/src/FirstTestClass.java
@@ -3,7 +3,7 @@ public class FirstTestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Second Test Class.");
+		System.out.println("First Test Class.");
 	}
 	
 }
